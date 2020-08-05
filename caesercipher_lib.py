dab59b11c3c7eae84e96437f7b930f5e4673fe93
K-subin <68969252+K-subin@users.noreply.github.com>
2020-08-06 00:32:18 +0900

    Modify word

--- caesercipher_lib.py
+++ caesercipher_lib.py
@@ -2,30 +2,30 @@ lower_alphabet = "abcdefghijklmnopqrstuvwxyz"
 upper_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 # 시저암호 함수
-def Caeser_encrypt(key, plain_msg):
+def Caesar_encrypt(key, plain_msg):
     cipher_msg = ''
-    for symbor in plain_msg:
-        if symbor in upper_alphabet:
-            symbor_idx = upper_alphabet.find(symbor)
-            cipher_msg = cipher_msg + upper_alphabet[(symbor_idx + key) % len(upper_alphabet)]
-        elif symbor in lower_alphabet: # 소문자인 경우
-            symbor_idx = lower_alphabet.find(symbor)
-            cipher_msg = cipher_msg + lower_alphabet[(symbor_idx + key) % len(lower_alphabet)]
+    for symbol in plain_msg:
+        if symbol in upper_alphabet:
+            symbol_idx = upper_alphabet.find(symbol)
+            cipher_msg = cipher_msg + upper_alphabet[(symbol_idx + key) % len(upper_alphabet)]
+        elif symbol in lower_alphabet: # 소문자인 경우
+            symbol_idx = lower_alphabet.find(symbol)
+            cipher_msg = cipher_msg + lower_alphabet[(symbol_idx + key) % len(lower_alphabet)]
         else:
-            cipher_msg = cipher_msg + symbor
+            cipher_msg = cipher_msg + symbol
     return cipher_msg
 
-def Caeser_decrypt(key, cipher_msg):
+def Caesar_decrypt(key, cipher_msg):
     recovered_msg = ''
-    for symbor in cipher_msg:
-        if symbor in upper_alphabet:
-            symbor_idx = upper_alphabet.find(symbor)
-            recovered_msg = recovered_msg + upper_alphabet[(symbor_idx - key) % len(upper_alphabet)]
-        elif symbor in lower_alphabet: # 소문자인 경우
-            symbor_idx = lower_alphabet.find(symbor)
-            recovered_msg = recovered_msg + lower_alphabet[(symbor_idx - key) % len(lower_alphabet)]
+    for symbol in cipher_msg:
+        if symbol in upper_alphabet:
+            symbol_idx = upper_alphabet.find(symbol)
+            recovered_msg = recovered_msg + upper_alphabet[(symbol_idx - key) % len(upper_alphabet)]
+        elif symbol in lower_alphabet: # 소문자인 경우
+            symbol_idx = lower_alphabet.find(symbol)
+            recovered_msg = recovered_msg + lower_alphabet[(symbol_idx - key) % len(lower_alphabet)]
         else:
-            recovered_msg = recovered_msg + symbor
+            recovered_msg = recovered_msg + symbol
     return recovered_msg
 
 #--------------------------------------------------------------------
@@ -33,11 +33,11 @@ def Caeser_decrypt(key, cipher_msg):
 # test main함수
 def main():
     plain_msg = "This is a plaintext message to be encrypted."
-    key = 3
-    cipher_msg = Caeser_encrypt(key, plain_msg)
+    key = 10
+    cipher_msg = Caesar_encrypt(key, plain_msg)
     print('PLAINTEXT =', plain_msg)
     print('CIPHERTEXT =', cipher_msg)
-    recovered_msg = Caeser_decrypt(key, cipher_msg)
+    recovered_msg = Caesar_decrypt(key, cipher_msg)
     print('RECOVEREDTEXT =', recovered_msg)
 
 # rnun main()
